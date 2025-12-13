cdd54f38bce08b63b24c626612a6d7ca53abc887
NANASANA IRFAN <139986589+irfan21381@users.noreply.github.com>
2025-12-13 23:45:42 +0530

    Update page.tsx

--- frontend/app/teacher/analytics/page.tsx
+++ frontend/app/teacher/analytics/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
 
-'use client'
+'use client';
+export const dynamic = "force-dynamic";
 
 import Layout from '@/components/AppShell'
 import { useQuery } from '@tanstack/react-query'
