eb3f3c71891e1a6cde0232e4d31c964581e4510f
NANASANA IRFAN <139986589+irfan21381@users.noreply.github.com>
2025-12-13 22:02:31 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,9 +5,9 @@ const nextConfig = {
     domains: ['localhost'],
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
   },
 }
 
 module.exports = nextConfig
-
