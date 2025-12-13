d663950a250b278447eccdec6a14cdad232d60ce
NANASANA IRFAN <139986589+irfan21381@users.noreply.github.com>
2025-12-13 22:07:55 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
-import { AuthProvider } from "./context/AuthContext";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "./components/ui/sonner"; // ✅ fixed import
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <ThemeProvider>
-        <App />
-        <Toaster />
-      </ThemeProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <App />
+      <Toaster />
+    </ThemeProvider>
   </React.StrictMode>
 );
+
